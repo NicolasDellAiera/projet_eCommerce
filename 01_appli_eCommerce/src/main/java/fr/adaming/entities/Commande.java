ddab9a3665ea6ecfc709a38066477c1385deba45
INTI-0366 <INTI-0366@DESKTOP-ODI5NOD>
2017-05-24 11:08:01 +0200

    salut

--- 01_appli_eCommerce/src/main/java/fr/adaming/entities/Commande.java
+++ 01_appli_eCommerce/src/main/java/fr/adaming/entities/Commande.java
@@ -1,5 +1,17 @@
 package fr.adaming.entities;
 
-public class Commande {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="commande")
+
+public class Commande implements Serializable {
+	
+	@Id
+	private int idCommande;
 
 }
