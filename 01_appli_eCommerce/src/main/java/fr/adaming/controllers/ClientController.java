aa02ec932a0bb74a4383c8f58654b92bb6f1b6b9
INTI-0366 <INTI-0366@DESKTOP-ODI5NOD>
2017-05-24 16:54:20 +0200

    un commentaire

--- 01_appli_eCommerce/src/main/java/fr/adaming/controllers/ClientController.java
+++ 01_appli_eCommerce/src/main/java/fr/adaming/controllers/ClientController.java
@@ -12,11 +12,18 @@ import fr.adaming.entities.Categorie;
 import fr.adaming.entities.Produit;
 import fr.adaming.service.ICategorieService;
 import fr.adaming.service.IProduitService;
+/**
+ * Cette classe est le controleur du client
+ * @author INTI-0366
+ *
+ */
+
 
 @Controller
 @RequestMapping("/site")
 
 public class ClientController {
+
 	
 	@Autowired
 	private ICategorieService catService;
