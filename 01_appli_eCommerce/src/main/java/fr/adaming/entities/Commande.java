a81303ae546083d8e6cebd8bb0c682bdfb83f158
INTI-0366 <INTI-0366@DESKTOP-ODI5NOD>
2017-05-24 11:09:16 +0200

    a

--- 01_appli_eCommerce/src/main/java/fr/adaming/entities/Commande.java
+++ 01_appli_eCommerce/src/main/java/fr/adaming/entities/Commande.java
@@ -2,7 +2,10 @@ package fr.adaming.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +15,8 @@ import javax.persistence.Table;
 public class Commande implements Serializable {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="idCommande")
 	private int idCommande;
 
 }
